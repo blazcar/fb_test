493b0d8bc56478e55e89b2bd8a9544465e16c004
Blaž Čar <bc0129@student.uni-lj.si>
2016-06-12 18:25:52 +0000

    Neki se je naredilo

---
+++ index.js
@@ -0,0 +1,20 @@
+function home_btn1(){
+    $("#home").click(function (){
+        $("#toggleContent").toggleClass("vidno");
+    })
+}
+function home_btn2(){
+    $("#about").click(function (){
+        $("#toggleContent").toggleClass("vidno");
+    })
+}
+function home_btn3(){
+    $("#links").click(function (){
+        $("#toggleContent").toggleClass("vidno");
+    })
+}
+function home_btn4(){
+    $("#contact").click(function (){
+        $("#toggleContent").toggleClass("vidno");
+    })
+}
